ac1995203051e7fe69aa2b280e07ce5c63cd21be
SouzaJanete <115745238+SouzaJanete@users.noreply.github.com>
2024-02-08 16:27:08 -0300

    Produto

---
+++ cypress/e2e/3-loja-ebac/produto.cy.js
@@ -0,0 +1,23 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+    
+    it('Deve selecionar um produto da lista', () => {
+        //cy.get('.block-inner')
+        cy.get('.products > .row')
+        //.first()
+        //.last()
+        //.eq(2)
+        .contains('Aero Daily Fitness Tee')
+        .click()
+
+        cy.get('#tab-title-description').should('contain' , 'Descrição')
+    });
+
+    
+
+});
